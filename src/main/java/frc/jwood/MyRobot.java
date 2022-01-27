c326b4f4e7e94865c8af39cface2c3da1358f22d
jwoodard <jwoodard@lpslancer.net>
2022-01-27 15:46:57 -0500

    First commit

---
+++ src/main/java/frc/jwood/MyRobot.java
@@ -0,0 +1,138 @@
+package frc.jwood;
+
+import java.lang.invoke.MethodHandles;
+
+public class MyRobot
+{
+    private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
+
+    // *** STATIC INITIALIZATION BLOCK ***
+    // This block of code is run first when the class is loaded
+    static
+    {
+        System.out.println("Loading: " + fullClassName);
+    }
+
+    // *** CLASS & INSTANCE VARIABLES ***
+
+
+
+    // *** CLASS CONSTRUCTOR ***
+    public MyRobot()
+    {
+
+    }
+
+    /**
+     * This method runs when the robot first starts up.
+     */
+    public void robotInit()
+    {
+
+    }
+
+    /**
+     * This method runs periodically (20ms) while the robot is powered on.
+     */
+    public void robotPeriodic()
+    {
+
+    }
+
+    /**
+     * This method runs one time when the robot enters autonomous mode.
+     */
+    public void autonomousInit()
+    {
+
+    }
+
+    /**
+     * This method runs periodically (20ms) during autonomous mode.
+     */
+    public void autonomousPeriodic()
+    {
+
+    }
+
+    /**
+     * This method runs one time when the robot exits autonomous mode.
+     */
+    public void autonomousExit()
+    {
+
+    }
+
+    /**
+     * This method runs one time when the robot enters teleop mode.
+     */
+    public void teleopInit()
+    {
+        
+    }
+
+    /**
+     * This method runs periodically (20ms) during teleop mode.
+     */
+    public void teleopPeriodic()
+    {
+        
+    }
+
+    /**
+     * This method runs one time when the robot exits teleop mode.
+     */
+    public void teleopExit()
+    {
+        
+    }
+
+    /**
+     * This method runs one time when the robot enters test mode.
+     */
+    public void testInit()
+    {
+
+    }
+
+    /**
+     * This method runs periodically (20ms) during test mode.
+     */
+    public void testPeriodic()
+    {
+
+    }
+
+    /**
+     * This method runs one time when the robot exits test mode.
+     */
+    public void testExit()
+    {
+
+    }
+
+    /**
+     * This method runs one time when the robot enters disabled mode.
+     */
+    public void disabledInit()
+    {
+
+    }
+
+    /**
+     * This method runs periodically (20ms) during disabled mode.
+     */
+    public void disabledPeriodic()
+    {
+
+    }
+
+    /**
+     * This method runs one time when the robot exits disabled mode.
+     */
+    public void disabledExit()
+    {
+
+    }
+
+}
